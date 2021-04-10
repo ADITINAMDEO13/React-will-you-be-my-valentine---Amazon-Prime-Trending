afb38fd4072771b43bc0e8f7302cbb44a8405d00
ADITINAMDEO13 <68394896+ADITINAMDEO13@users.noreply.github.com>
2021-04-10 20:54:45 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,57 +1,52 @@
-import React, {Component, useEffect, useState} from "react";
+import React, {Component, useState} from "react";
 import '../styles/App.css';
+// import slides from '../data';
 
-const App = (props) => {
-  const [index,setindex]=useState(0);
-
-  const disable=(id)=>{
-    // document.getElementById(id).removeAttribute("disabled");
-    document.getElementById(id).setAttribute("disabled",true)
-  } 
-  const enable =(id)=>{
-    document.getElementById(id).removeAttribute("disabled");
-    // document.getElementById(id).setAttribute("disabled",false)
-  }
-  const handleNext=()=>{
-    if(index+1<props.slides.length){
-      setindex(index+1);
-    }
-    if(index+2==props.slides.length){
-      disable("next");
-    }
-    enable("prev");
-    enable("restart");
-  }
-  const handleRestart=()=>{
-    setindex(0);
-    disable("prev");
-    disable("restart");
-    enable("next");
-  }
-  const handlePrev=()=>{
-    if(index>=1)
-    setindex(index-1);
-    if(index==1){
-      disable("prev");
-      disable("restart");
-      enable("next");
+const App = ({slides}) => {
+  const [count,setCount] = useState(0);
+  const [next, setNext] = useState(false);
+  const [prev, setPrev] = useState(true);
+  const [restart, setRestart] = useState(true);
+  const handleNext = () =>{
+      setPrev(false);
+      setRestart(false);
+    if(count<5){
+      setCount(count+1);
+    }if(count==3){
+      setNext(true);
     }
   }
-  useEffect(()=>{
-    if(index==0){
-      disable("prev");
-    disable("restart");
+  const handlePrev = () =>{
+    if(count>0)
+      setCount(count-1);
+    if(count==1){
+      setPrev(true);
+      setRestart(true);
     }
-  })
+    setNext(false);
+    
+  }
+  const handleRestart = () =>{
+    setCount(0);
+    setNext(false);
+    setPrev(true);
+    setRestart(true);
+  }
   return (
     <>
-      <h1 data-testid="title">{props.slides[index].title}</h1>
-      <p data-testid="text">{props.slides[index].text}</p>
-      <button id="next" data-testid="button-next" onClick={handleNext} >Next</button>
-      <button id="prev" data-testid="button-prev" onClick={handlePrev} disabled={false}>Prev</button>
-      <button id="restart" data-testid="button-restart" onClick={handleRestart} disabled={false}>Restart</button>
+    <div id="slide">
+      <h1 data-testid="title">{slides[count].title}</h1>
+      <p data-testid="text">{slides[count].text}</p>
+      </div>
+      <div>
+          <button disabled={prev} data-testid="button-prev" onClick={handlePrev}>Prev</button>
+          <button disabled={restart} data-testid="button-restart" onClick={handleRestart}>Restart</button>
+          <button disabled={next} data-testid="button-next" onClick={handleNext}>Next</button>
+      </div>
     </>
   )
 }
 
+
 export default App;
+
